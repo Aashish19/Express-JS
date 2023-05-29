b814017b621e4683cb5b633d04e430b2d8299b2f
Aashish Gamangatti <aashishgamanagatti@gmail.com>
2023-05-29 16:24:10 +0530

    Simpler code

--- controllers/products.js
+++ controllers/products.js
@@ -17,12 +17,14 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    productCSS: true,
-    activeShop: true,
+  Product.fetchAll((products) => {
+    res.render("shop", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+      hasProducts: products.length > 0,
+      productCSS: true,
+      activeShop: true,
+    });
   });
 };
