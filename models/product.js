e162b8735f37c4951ff6deef92012b4988796881
Aashish Gamangatti <aashishgamanagatti@gmail.com>
2023-06-13 21:22:07 +0530

    Edit and Delete Item

--- models/product.js
+++ models/product.js
@@ -1,7 +1,11 @@
-const path = require("path");
 const fs = require("fs");
+const path = require("path");
 
-const p = path.join(require.main.filename, "data", "product.json");
+const p = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
 
 const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
@@ -14,15 +18,18 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
     getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log("err", err);
+        console.log(err);
       });
     });
   }
